ee8256f93bb22568dcd5ce8da0cc8201504629fd
Torremolinos <adrian_2054@hotmail.com>
2023-09-25 00:10:42 +0200

    añandiendo estilos y js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,14 +1,14 @@
 function calcular() {
   const num1 = parseFloat(document.getElementById("first").value);
   const num2 = parseFloat(document.getElementById("seccond").value);
-  const eleccion = document.getElementById("choice").value;
+  const choice = document.getElementById("choice").value;
   let resultado;
-  switch (eleccion) {
+  switch (choice) {
     case "add":
       resultado = num1 + num2;
       break;
 
-    case "subtract":
+    case "substract":
       resultado = num1 - num2;
       break;
 
@@ -17,7 +17,25 @@ function calcular() {
       break;
 
     case "divide":
-      num1 && num2 === 0 ? alert('No se puede dividir por 0') : resultado = num1 / num2;
+      num1==0 && num2 === 0 ? alert('No se puede dividir por 0') : resultado = num1 / num2;
       break;
+      default:
+        resultado= 'Operacion invalida';
   }
+  document.getElementById('result').textContent = `El resultado es: ${resultado}`;
 }
+
+function resetForm() {
+  document.getElementById('calculator-form').reset();
+  document.getElementById('result').textContent = '';
+}
+// Manejar el envío del formulario
+document.getElementById('calculator-form').addEventListener('submit', function (e) {
+  e.preventDefault(); // Evitar el envío del formulario
+  calculate();
+});
+
+// Manejar el botón de reinicio
+document.getElementById('reset').addEventListener('click', function () {
+  resetForm();
+});
